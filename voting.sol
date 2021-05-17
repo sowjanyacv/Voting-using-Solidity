1537527414828c809ea0be274e61cf4e24446b81
Sowjanya C V <47071519+sowjanyacv@users.noreply.github.com>
2021-05-17 19:36:15 +0530

    Update voting.sol

--- voting.sol
+++ voting.sol
@@ -1,8 +1,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 /** 
- * @title Ballot
- * @dev Implements voting process along with vote delegation
+ * Implements voting process along with vote delegation
  */
 contract Ballot {
    
